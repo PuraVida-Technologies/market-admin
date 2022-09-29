a5a3a985a0bc9a75b9894706c61df7d18ab3d5f1
Milad Ezzat <miladezzat.f@gmail.com>
2022-09-30 00:07:25 +0200

    Feature/add header (#1)
    * feat: add assets
    
    * chore: install deps
    
    * feat: add main layout
    
    * feat: add top menu
    
    * style: add global styles
    
    * feat: add template card
    
    * feat: add grid on home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,32 @@
+import Card from "@/components/Card";
 import MainLayout from "@/components/layouts/Main";
+import { Col, Row } from "antd";
 import { GetServerSideProps, NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <MainLayout>
-      <p>Home</p>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={6}>
+          <Card />
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Card />
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Card />
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <Card />
+        </Col>
+      </Row>
     </MainLayout>
   );
 };
