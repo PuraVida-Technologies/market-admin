65a1d3f3584d32c91d6671db9c06dd70e4764be2
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-09-30 07:00:30 +0200

    fix: clear

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,19 +1,24 @@
 import "antd/dist/antd.css";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import { DatePicker, Space } from "antd";
 import Image from "next/image";
 
 export default function CustomDatePicker(): JSX.Element {
+  const [date, setDate] = useState<string | undefined>(undefined);
+
   return (
     <Space direction="vertical">
       <DatePicker
-        suffixIcon={<Image src="/assets/arrow-down.svg" alt="arrow" width={16} height={9} />}
+        suffixIcon={!date && <Image src="/assets/arrow-down.svg" alt="arrow" width={16} height={9} />}
         className={styles.datePicker}
         inputReadOnly
-        allowClear={false}
         placeholder="Recent Updated list"
         bordered={false}
+        onChange={(moment) => {
+          setDate(moment?.format("YYYY-MM-DD"));
+        }}
+        allowClear={date ? true : false}
       />
     </Space>
   );
