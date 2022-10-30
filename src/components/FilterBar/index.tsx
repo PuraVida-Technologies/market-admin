2f097858cd5e83a84159a96be0f29bdf885e028d
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-10-30 20:20:21 +0200

    feat: use new component

--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -3,56 +3,55 @@ import React from "react";
 import styles from "./styles.module.scss";
 import CustomDatePicker from "../DatePicker";
 import Bubble from "../Bubble";
-
 import { Button, Dropdown, Menu } from "antd";
+import { DASHBOARD } from "@/common/constants";
 
-const menu = (
-  <Menu
-    items={[
-      {
-        key: "1",
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-            Map
-          </a>
-        ),
-      },
-      {
-        key: "2",
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-            Post
-          </a>
-        ),
-      },
-      {
-        key: "3",
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-            Tag
-          </a>
-        ),
-      },
-    ]}
-  />
-);
+type FilterBarProps = {
+  handleView: (view: string) => void;
+};
 
-export default function FilterBar(): JSX.Element {
+export default function FilterBar({ handleView }: FilterBarProps): JSX.Element {
+  const viewsMenu = (
+    <Menu
+      items={[
+        {
+          key: "1",
+          label: <span>Post</span>,
+          onClick: () => handleView(DASHBOARD.views.post),
+        },
+        {
+          key: "2",
+          label: <span>Tags</span>,
+          onClick: () => handleView(DASHBOARD.views.tag),
+        },
+        {
+          key: "3",
+          label: <span>Map</span>,
+          onClick: () => handleView(DASHBOARD.views.map),
+        },
+        {
+          key: "4",
+          label: <span>Request Post Update</span>,
+          onClick: () => handleView(DASHBOARD.views.updatePostRequest),
+        },
+      ]}
+    />
+  );
+
   return (
     <div className={styles.filterBAr}>
       <CustomDatePicker />
       <div className={styles.bubblesContainer}>
-        <Dropdown overlay={menu} placement="bottomLeft" arrow>
+        <Dropdown overlay={viewsMenu} placement="bottomLeft" arrow trigger={["click"]}>
           <Button style={{ border: "none", padding: "0", outline: "none" }}>
             <Bubble text="listing" src="/icons/listing.svg" iconWidth={16} iconHeight={14} />
           </Button>
         </Dropdown>
-        <Dropdown overlay={menu} placement="bottomLeft" arrow>
+        <Dropdown overlay={viewsMenu} placement="bottomLeft" arrow trigger={["click"]}>
           <Button style={{ border: "none", padding: "0", outline: "none" }}>
             <Bubble text="sorting" src="/icons/sorting.svg" iconWidth={24} iconHeight={15} />
           </Button>
         </Dropdown>
-        {/* <Bubble text="sorting" src="/icons/sorting.svg" iconWidth={24} iconHeight={15} /> */}
       </div>
     </div>
   );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,101 +1,44 @@
-import { Col, Row, Pagination } from "antd";
-import { customStyles } from "@/util/modalStyle";
-import { getPostDetails, Post, postAdminService, PostsResponse } from "@/services/post";
 import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
-import { NextRouter, useRouter } from "next/router";
-import CustomCard from "@/components/CustomCard";
 import FilterBar from "@/components/FilterBar";
 import MainLayout from "@/components/layouts/Main";
-import ProductModal from "@/components/modals/ProductModal";
-import React, { useEffect, useState } from "react";
+import PostView from "@/components/Post";
+import { useEffect, useState } from "react";
+import { DASHBOARD } from "@/common/constants";
+import { NextRouter, useRouter } from "next/router";
+import TagView from "@/components/Tag";
+import MapView from "@/components/Map";
+import PostUpdateRequestView from "@/components/PostUpdateRequest";
 
-const Dashboard: NextPage = ({
-  total,
-  page,
-  pageSize,
-}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
+const Dashboard: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
+  const { total, page, pageSize, defaultView } = props;
+  const [view, setView] = useState(defaultView);
   const router: NextRouter = useRouter();
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [postDetails, setPostDetails] = useState<Post>({});
-  const [posts, setPosts] = useState<PostsResponse>();
 
-  function onChangePagination(page: number, pageSize: number) {
-    console.log({ page, pageSize });
-    if (page) {
-      router.query.page = `${page}`;
+  function handleView(view: string) {
+    if (Object.values(DASHBOARD.views).includes(view)) {
+      setView(view);
+      router.query.view = view;
+      router.push(router, undefined, { shallow: true });
     }
-
-    if (pageSize) {
-      router.query.pageSize = `${pageSize}`;
-    }
-
-    router.push(router);
   }
 
-  function openModal(id?: string) {
-    if (id) {
-      router.query.id = id;
-      router.query.isModalOpen = "true";
-      router.push(router);
-    }
-
-    setIsOpen(true);
-  }
+  const Views = {
+    [DASHBOARD.views.post]: <PostView page={page} total={total} pageSize={pageSize} />,
+    [DASHBOARD.views.tag]: <TagView />,
+    [DASHBOARD.views.map]: <MapView />,
+    [DASHBOARD.views.updatePostRequest]: <PostUpdateRequestView />,
+  };
 
   useEffect(() => {
-    if (router.query.id && router.query.isModalOpen) {
-      setIsOpen(true);
-      getPostDetails(router.query.id as string).then((_postDetails) => setPostDetails(_postDetails));
+    if (Object.values(DASHBOARD.views).includes(view)) {
+      setView(view);
     }
-  }, [router]);
-
-  function closeModal() {
-    // 👇️ delete each query param
-    delete router.query["id"];
-    delete router.query["isModalOpen"];
-
-    // 👇️ update state after
-    router.push(router, undefined, { shallow: true });
-
-    setIsOpen(false);
-  }
-
-  useEffect(() => {
-    postAdminService({ limit: pageSize, page }).then((data) => {
-      setPosts(data);
-    });
-  }, [pageSize, page]);
+  }, [view]);
 
   return (
     <MainLayout>
-      <FilterBar />
-      <Row gutter={[16, 16]}>
-        {posts?.data?.map((post) => {
-          return (
-            <Col className="gutter-row" span={6} xs={24} sm={12} md={12} lg={8} xl={6} key={post._id}>
-              <CustomCard viewDetail={openModal} post={post} />
-            </Col>
-          );
-        })}
-      </Row>
-      <div style={{ display: "flex", justifyContent: "center", marginBottom: "2rem", marginTop: "1rem" }}>
-        <Pagination
-          className="pagination"
-          defaultCurrent={page}
-          defaultPageSize={pageSize}
-          total={posts?.pagination?.total || total}
-          responsive
-          onChange={onChangePagination}
-          pageSizeOptions={[12, 24, 48, 96]}
-        />
-      </div>
-      <ProductModal
-        postDetails={postDetails}
-        modalIsOpen={modalIsOpen}
-        closeModal={closeModal}
-        customStyles={customStyles}
-      />
-      ;
+      <FilterBar handleView={handleView} />
+      {Views[view]}
     </MainLayout>
   );
 };
@@ -107,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSideP
       total: query.total ? +query.total : 1,
       page: query.page ? +query.page : 1,
       pageSize: query.pageSize ? +query.pageSize : 12,
+      defaultView: query.view ? query.view : DASHBOARD.views.post,
     },
   };
 };
